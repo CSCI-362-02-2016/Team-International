6d22bb04f79c036aec618bb39c43027c0cbbdb89
ttang725 <ttang725@gmail.com>
2016-09-21 01:18:58 -0400

    Add files via upload
    sample test driver with math library

---
+++ practice/testDriverSquare.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <"mathlib.h">
+
+
+int main()
+{
+   cout << "Hello World"; // prints Hello World
+   
+   int a = 2;
+   cout << "The output of the square of a is: " << square(a) << endl;
+   
+   return 0;
+}
