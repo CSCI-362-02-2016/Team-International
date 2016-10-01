54ab34add1933914990bbab06d2dc7b21bb336bd
gui <gui529@hotmail.com>
2016-10-01 00:08:47 -0400

    new files

--- Gui_TestPlace/testScript.sh
+++ Gui_TestPlace/testScript.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 # My first script
+ 
 
-#Change font
-echo "<font face = "Helvetica"><b>TEAM INTERNATIONAL</b></font>" >> myList.html
+ARRAY=()
 
+#! /bin/bash
+cat testCase1 | while read LINE
+do
+echo $LINE
+ARRAY+=($LINE)
 
-# Part of code from unix.stackexchange.com/questions/86722/how-do-i-loop-through-only-directories-in-bash
-
-#loops through all root directories and paste teir name into myList.html
-for d in *; 
-	do
-	echo "<font face = "Helvetica"><br>$d</font>" >> myList.html
+echo "${ARRAY[2]}"
 done
 
+echo ${ARRAY[3]}
 
-#Sets permission so we can run the script
-chmod 755 myList.html
-
-#Open myList.html in a browser.
-xdg-open myList.html
