da213bc0e5bfd98b15e94db283acbbba7357f0f9
ttang725 <ttang725@gmail.com>
2016-10-21 02:34:10 -0400

    Add files via upload

--- practice/testDriverCube.cpp
+++ practice/testDriverCube.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include "mathlib.h"
-using namespace std;
-int main()
-{
-	
-  
-  cout<< "Starting cube method testing..."<< endl;
-  int a = 2;
-  int c = cube(a);
 
-  cout << "The cube of 2 is : " << c << endl;
+int main(int argc, char* argv[]) {
+
+	using namespace std;
+	int a = atoi(argv[1]);
+	//int b = atoi(argv[2]);
+	int retval = cube(a);
+	cout << retval << endl; //return the cube of the argument
+
+
+
+
 }
