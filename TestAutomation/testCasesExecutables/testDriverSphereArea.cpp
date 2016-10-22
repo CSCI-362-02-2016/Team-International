1632ddb49c8d4000ac114b1918cec41aaa359978
ubuntu <ttang725@gmail.com>
2016-10-21 21:47:44 -0400

    added sphereArea test case 3

---
+++ TestAutomation/testCasesExecutables/testDriverSphereArea.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "mathlib.h"
+
+int main(int argc, char* argv[]) {
+
+	using namespace std;
+	int a = atoi(argv[1]);
+	//int b = atoi(argv[2]);
+	int retval = sphereArea(a);
+	cout << retval << endl; //return area of the sphere
+
+
+
+
+}
