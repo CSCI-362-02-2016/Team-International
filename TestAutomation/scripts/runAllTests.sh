3e8c27d01dd83b73700f93bcb1cc5bc8a7205046
landaumd <megan.d.landau@gmail.com>
2016-10-25 12:26:43 -0400

    edited script so for better readability

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -18,44 +18,50 @@ echo "<table border ="1px">
 	" > ../reports/report.html
 
 
-#for i in $(ls ../testCases -1 | wc -l); do  # ls the /TestCases dir and loop all files.
-for i in $(ls ../testCases); do  # ls the /TestCases dir and loop all files.
+#Loop through all the test cases .txt files in the testCases folder
+for i in $(ls ../testCases); do
 
-readarray -t LINES < ../testCases/$i
+	readarray -t LINES < ../testCases/$i
 
-#Read testCase[i].txt and save into variables#
-TESTCASE=${LINES[10]} 
-REQUIREMENT=${LINES[11]}
-COMPONENT=${LINES[12]}
-DRIVER=${LINES[13]}
-METHOD=${LINES[14]}
-INPUT=${LINES[15]}
-EXPECTED_OUTPUT=${LINES[16]}
+	#Read testCase[i].txt and save into variables#
+	TESTCASE=${LINES[10]} 
+	REQUIREMENT=${LINES[11]}
+	COMPONENT=${LINES[12]}
+	DRIVER=${LINES[13]}
+	METHOD=${LINES[14]}
+	INPUT=${LINES[15]}
+	EXPECTED_OUTPUT=${LINES[16]}
 
-cd ../testCasesExecutables #Switch to Driver Directory
+	#Switch to Driver Directory
+	cd ../testCasesExecutables
+	
+	#Compile the driver *${DRIVER::-4} shave off .cpp extension*
+	g++ $DRIVER -o ${DRIVER::-4} 
 
-g++ $DRIVER -o ${DRIVER::-4}  #Compile the driver *${DRIVER::-4} shave off .cpp extension*
+	#Run the driver and save return to $RESULT.
+	RESULT=$(./${DRIVER::-4} $INPUT)
+	
+	#Print each variable to the terminal	
+	echo "############### TEST RESULT ################"
+	echo "Test Case ID:		$TESTCASE"
+	echo "Requirement:		$REQUIREMENT"
+	echo "Driver:			$DRIVER"
+	echo "Input:			$INPUT"
+	echo "Expected Output:	$EXPECTED_OUTPUT"
+	echo "Actual Output:		$RESULT"
+	
+	#Compare the result to the expected output
+	if [ $RESULT "==" $EXPECTED_OUTPUT ]; then
+	RESULT_OUTCOME="pass"
+	echo "Test Result:          	$RESULT_OUTCOME"
+	else
+	RESULT_OUTCOME="fail"
+	echo "Test Result:		$RESULT_OUTCOME"
+	fi
 
-RESULT=$(./${DRIVER::-4} $INPUT) #Run the driver and save return to $RESULT.
-echo "############### TEST RESULT ################"
-echo "Test Case ID:		$TESTCASE"
-echo "Requirement:		$REQUIREMENT"
-echo "Driver:			$DRIVER"
-echo "Input:			$INPUT"
-echo "Expected Output:	$EXPECTED_OUTPUT"
-echo "Actual Output:		$RESULT"
+	#Populate table with results
 
-if [ $RESULT "==" $EXPECTED_OUTPUT ]; then
-RESULT_OUTCOME="pass"
-echo "Test Result:          	$RESULT_OUTCOME"
-else
-RESULT_OUTCOME="fail"
-echo "Test Result:		$RESULT_OUTCOME"
-fi
-
-###Populate table with results
-
-echo "<tr><td>$TESTCASE</td><td>$METHOD</td><td>$INPUT</td><td>$EXPECTED_OUTPUT</td><td>$RESULT</td><td>$RESULT_OUTCOME</td>                       </tr>" >> ../reports/report.html
+	echo "<tr><td>$TESTCASE</td><td>$METHOD</td><td>$INPUT</td><td>$EXPECTED_OUTPUT</td><td>$RESULT</td><td>$RESULT_OUTCOME</td>                       </tr>" >> ../reports/report.html
 
 done #end of for loop
 
--- TestAutomation/testCasesExecutables/testDriverSquare.cpp
+++ TestAutomation/testCasesExecutables/testDriverSquare.cpp
@@ -5,6 +5,7 @@ int main(int argc, char* argv[])
 {
 	using namespace std;
 	int a = atoi(argv[1]);
+	//cout << argv[0] <<endl;
 	//int b = atoi(argv[2]);
 	int retval = square(a);
 	cout << retval <<endl; //return the square of the argument
