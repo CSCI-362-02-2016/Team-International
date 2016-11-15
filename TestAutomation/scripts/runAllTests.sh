bf1bbd57f7f37023d0eae89c7a338e611947b3b6
landaumd <megan.d.landau@gmail.com>
2016-11-14 22:12:58 -0500

    changed html style dramatically

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -2,51 +2,83 @@
 
 cd ./scripts
 
-#Create header of result table
-echo "<h2>TEAM INTERNATIONAL</h2>
-	<font face = "Helvetica"><style>
+echo "<html>" > ../reports/report.html
+
+echo "<img src="flags.png">" >> ../reports/report.html
+
+#Create intro and header of result table
+echo "	<h2>Team International Test Results</h2>
+	<hr noshade size="1" color="#EAEAEA">
+	<h3>Team Members: </h3> 
+	<p>Gui Costa, Tony Tang, Megan Landau</p>
+	<h3>Chosen H/FOSS Project:</h3>
+	<p>Celestia-G2</p>
+	<h3>Description of Results: </h3>	
+	<p>The table below displays the results of 25 test cases that were used to analyze Celestia-G2's math library. By creating and using custom drivers for each of the five methods tested, we were able to compare actual outcomes with expected results that we calculated ourselves. Inputs tested include small, large, negative, positive, extremely large, decimal, integer, and zero values. Pass or fail results are displayed in green and red, respectively.</p>
+	<style>
 	h2	{
-		color:blue;
-		font-face: "Helvetica";
+		text-align: center;
+		color: #525252;
+		font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+		font-size: 30px;
+	}
+	h3	{
+		color: #525252;
+		font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+		font-weight: bold;
+		font-size: 16px;
+	}
+	img {
+		display: block;		
+		width: 60%;
+		margin: auto;
+	}
+	p {
+		margin-right: 40px;
+		margin-left: 40px;
+		line-height: 20px;	
+	}
+	body	{
+		padding: 50px 70px 50px 70px;
+		font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+		color: #525252;
+		line-height: 20px;	
 	}
 	table {
-		border: 1px solid black;
 		border-collapse: collapse;
 		width: 100%;
 	}
 	td {
-		border: 1px solid #dddddd;
-		test-align: left;
+		text-align: left;
 		padding: 10px;
 	}
 	th {
-		border: 1px solid #dddddd;
-		test-align: left;
+		text-align: left;
 		padding: 10px;
 		color: white;
-		background-color: #4CAF50;
+		background-color: #858585;
 	}
-	tr:nth-child(even){
-	background-color: #dddddd;
+	tr:nth-child(even) {
+		background-color: #EAEAEA;	
 	}
 	</style>
 
 	<table>
 	<tr>
-		<th>TestCaseID</th>
+		<th>Test Case ID</th>
 		<th>Method</th>
 		<th>Input</th>
-		<th>Expected Outcome</th>
+		<th>Expected Output</th>
 		<th>Actual Output</td>
-		<th> Pass/ Fail</th>
+		<th>Pass / Fail</th>
 	</tr>
 	<tr>
-
-		
-	
 	</tr></font>
-	" > ../reports/report.html
+	" >> ../reports/report.html
 
+#Initialize variables for bar graph
+PASS=0
+FAIL=0
 
 #Loop through all the test cases .txt files in the testCases folder
 for i in $(ls ../testCases); do
@@ -80,20 +112,26 @@ for i in $(ls ../testCases); do
 	echo "Expected Output:	$EXPECTED_OUTPUT"
 	echo "Actual Output:		$RESULT"
 	
-	#
+
+	#Compare values to determine Pass or Fail outcome and populate table with results
 	if [ $RESULT "==" $EXPECTED_OUTPUT ]; then
-	RESULT_OUTCOME="pass"
-	echo "Test Result:          	$RESULT_OUTCOME"
+		((PASS += 1))
+		RESULT_OUTCOME="Pass"
+		echo "Test Result:          	$RESULT_OUTCOME"
+		echo "<tr><td>$TESTCASE</td><td>$METHOD</td><td>$INPUT</td><td>$EXPECTED_OUTPUT</td><td>$RESULT</td><td 		style="color:green">$RESULT_OUTCOME</td></tr>" >> ../reports/report.html
 	else
-	RESULT_OUTCOME="fail"
-	echo "Test Result:		$RESULT_OUTCOME"
+		((FAIL += 1))
+		RESULT_OUTCOME="Fail"
+		echo "Test Result:		$RESULT_OUTCOME"
+		echo "<tr><td>$TESTCASE</td><td>$METHOD</td><td>$INPUT</td><td>$EXPECTED_OUTPUT</td><td>$RESULT</td><td 		style="color:red">$RESULT_OUTCOME</td></tr>" >> ../reports/report.html
 	fi
 
-	#Populate table with results
-#<font color="red">$RESULT_OUTCOME</font>
-	echo "<tr><td>$TESTCASE</td><td>$METHOD</td><td>$INPUT</td><td>$EXPECTED_OUTPUT</td><td>$RESULT</td><td>$RESULT_OUTCOME</td>                       </tr>" >> ../reports/report.html
-
 done #end of for loop
+
+#echo "PASS = $PASS"
+#echo "FAIL = $FAIL"
+
+echo "</html>" >> ../reports/report.html
 
 xdg-open ../reports/report.html
 
